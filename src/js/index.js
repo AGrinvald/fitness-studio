005f5a97d4995e6f7a5397b4c57001da77a0d8a7
AGrinvald <grinvald.anastassiya@gmail.com>
2020-09-21 20:19:42 +0600

    1

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ function insertViewport() {
         || document.body.clientWidth;
 
     if (w <= 1024) {
-        return viewport_meta.setAttribute('content', "user-scalable=yes, width=1400");
+        return $('meta[name=viewport]').attr('content', 'user-scalable=yes, width=1400');
     } else if (w <= 1920) {
         return $('meta[name=viewport]').attr('content', 'user-scalable=yes, initial-scale=0.67 width=1920');
     } else {
